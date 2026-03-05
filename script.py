eba8db6f33fc3861ff53613ae9556edd3c9fd9ab
Abdelrahim-bebo <abdelrahimbebo@gmail.com>
2026-03-05 13:34:07 +0200

    Created The Python Script

---
+++ script.py
@@ -0,0 +1,11 @@
+# Simple Task Tracker
+tasks = ["Setup Git", "Create SSH Key", "Write Script"]
+
+print("--- My To-Do List ---")
+
+for index, task in enumerate(tasks, start=1):
+    print(f"{index}. {task}")
+
+new_task = "Learn Branching"
+tasks.append(new_task)
+print(f"Added: {new_task}")
